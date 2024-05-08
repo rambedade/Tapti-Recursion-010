0006b5cd6bf70e6977840bc1320cab15a863a474
adityaks-lts <aditya432001@gmail.com>
2024-05-08 23:19:48 +0530

    added signup functionatlity

---
+++ Frontend/scripts/login.js
@@ -0,0 +1,10 @@
+let userEmail = document.getElementById("user_email");
+let userPassword = document.getElementById("user_password");
+let submitLogin = document.getElementById("submit-login");
+let userURL = "https://tapti-recursion-010-v93f.onrender.com/users";
+
+submitLogin.addEventListener("click", verifyUserData);
+
+await function verifyUserData(){
+
+}
