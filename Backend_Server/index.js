eda7bf9796e31ae9aa820087be420cbe5e4edd51
adityaks-lts <aditya432001@gmail.com>
2024-05-08 13:33:52 +0530

    server add

--- Backend_Server/index.js
+++ Backend_Server/index.js
@@ -1,10 +1,17 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
+let cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+server.db = router.db;
+
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`JSON Server with authentication is running on port ${port}`);
+});
