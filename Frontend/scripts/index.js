91c31c8b844ab7c4ae4b2320f57210dccf3e4bd3
rambedade <rambedade2308@gmail.com>
2024-05-09 23:44:38 +0530

    added gateway section

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -12,3 +12,24 @@ const dropdownMenu = document.getElementById('dropdownMenu');
 menuIcon.addEventListener('click', function() {
   dropdownMenu.classList.toggle('show');
 });
+
+// Rameshwar /
+
+document.addEventListener("DOMContentLoaded", function() {
+    var popularDropdown = document.getElementById("popularDropdown");
+    popularDropdown.classList.add("active");
+    var popularContent = popularDropdown.querySelector(".dropdown-content");
+    popularContent.style.display = "block";
+
+    var dropdowns = document.querySelectorAll(".dropdown");
+    dropdowns.forEach(function(dropdown) {
+        dropdown.addEventListener("click", function() {
+            var currentContent = document.querySelector(".dropdown.active .dropdown-content");
+            if (currentContent && currentContent.parentElement !== this) {
+                currentContent.parentElement.classList.remove("active");
+                currentContent.style.display = "none";
+            }
+            this.classList.toggle("active");
+        });
+    });
+});
